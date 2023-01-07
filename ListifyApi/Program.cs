9ef627afad9bb5cdf6941e3c8c6d52b02d3fbf1b
zaarix <zaarix.dev@gmail.com>
2023-01-06 16:43:34 -0800

    added validation and json responses

--- ListifyApi/Program.cs
+++ ListifyApi/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/listify", (int begin, int end, int index) => {
+app.MapGet("/listify", (int begin, int end, int index) =>
+{
+    if (begin >= end)
+        return Results.BadRequest(new { message = "end must be greater than begin" });
+
+    if (index < 0 || index > (end - begin))
+        return Results.BadRequest(new { message = "index out of range"});
+
     var list = new ListifyLib.Listify(begin, end);
-    return list[index];
+    return Results.Ok(new { value = list[index] });
 });
 
 app.Run();
